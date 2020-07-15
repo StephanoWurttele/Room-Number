c2554f2f581e06e9a43cfbccc2ae2330e734bf53
StephanoWurttele <stephano.wurttele@utec.edu.pe>
2020-07-14 23:17:12 -0500

    Program with GUI

--- graphic-room-number.py
+++ graphic-room-number.py
@@ -22,13 +22,11 @@ PARSING_HABILES = ["Parsing_h4b1l3s"]
 ALL = [False]
 
 def obtenerPorcentajes():
-  CURSO.append(input("Ingrese nombre del curso"))
-  MAX_POR_SALON.append(int(input("Numero de alumnos por sección en el curso:")))
-  NOTAS.append(int(input("Ingrese el numero de notas en el curso de acuerdo a Intranet\nNumero: ")))
-  print("Ingrese el peso de las notas en formato decimal. (Si vale 10%, colocar 0.1)")
-  for i in range(NOTAS[0]):
-    print("Nota",i+1,end = ": ")
-    PORCENTAJES.append(float(input()))
+  CURSO.append(courseName.get())
+  MAX_POR_SALON.append(int(maxStudents.get()))
+  NOTAS.append(int(numberGrades.get()))
+  for i in range(len(notasInput)):
+    PORCENTAJES.append(float(notasInput[i][1].get()))
 
 def promedio(notas):
   prom = 0
@@ -179,7 +177,6 @@ def parseData(file_name, all):
     print("RETIRADOS ", retirados, " ALUMNOS")
     TOTAL.append(aprobados)
 
-
 def displayData():
   for i in HISTORIAL_NOTAS:
     print("NUEVO CICLO")
@@ -246,19 +243,16 @@ def run():
     parseHabiles("./CSVs/" + PARSING_HABILES[0] +".csv")
     # produceCSV("CSVs/test_Estadistica_2019_1.csv", PRODUCIDO)
 
-    total = int(input("Ingrese el número total de alumnos habilitados para el actual ciclo: "))
-    TOTAL.append(total)
-    TOTAL[1] += int(input("Ingrese el numero aproximado de alumnos que estaran habilitados el siguiente ciclo"))
-    file_name = input("Ingrese nombre de archivo CSV: ")
+    TOTAL.append(int(numberHabiles.get()))
+    TOTAL[1] += int(numberNuevos.get())
+    file_name = "test_Estadistica_2019_1"
     all_grades = parseStudentGrades("CSVs/"+file_name+".csv")
     for student in all_grades:
       rate = getRate(student)
       student.append(rate)
-    produceXLSX("prediccion_"+file_name+".xlsx", all_grades)
-
+    produceXLSX("prediccion_"+file_name+"GUI.xlsx", all_grades)
 
 def parseAndClose():
-  print(habiles_file_name.get())
   parse()
   top.destroy()
 
@@ -285,54 +279,182 @@ def openParse():
   habiles_file_name.pack()
   submit.pack()
 
+def showSubmit():
+  habiles_text.pack_forget()
+  nuevos_text.pack_forget()
+  mensaje_nuevos.pack_forget()
+  mensaje_habiles.pack_forget()
+  numberNuevos.pack_forget()
+  numberHabiles.pack_forget()
+  submit.pack_forget()
+  habiles_text.pack(pady=(20,5))
+  numberHabiles.pack()
+  mensaje_habiles.pack()
+  nuevos_text.pack(pady=(20,5))
+  numberNuevos.pack()
+  mensaje_nuevos.pack()
+  submit.pack()
+  temp = True
+  x = 0
+  for i in notasInputVars:
+    if(i[1].cget("text") != " "):
+      temp = False
+      break
+    x+=float(i[0].get())
+  if x != 1:
+    temp = False
+  if(mensaje_notas.cget("text") == " " and mensaje_alumnos.cget("text") == " " and temp and mensaje_nuevos.cget("text") == " " and mensaje_habiles.cget("text") == " "):
+    submit["state"] = "normal"
+  else:
+    submit["state"] = "disabled"
+
+def percentageCheck():
+    for i in notasInputVars:
+      try:
+        float(i[0].get())
+        if(i[0].get().isdigit()):
+          i[1].config(text="Por favor, ingrese un numero decimal en el porcentaje de nota")
+        else:
+          i[1].config(text=" ")
+      except ValueError:
+        i[1].config(text="Por favor, ingrese un numero decimal en el porcentaje de nota")
+    showSubmit()
+
+def habilesNumber():
+  try:
+    int(sv_habiles.get())
+    mensaje_habiles.config(text=" ")
+  except ValueError:
+    mensaje_habiles.config(text="Por favor, ingrese un numero en el numero de alumnos por salon")
+  showSubmit()
+
+def nuevosNumber():
+  try:
+    int(sv_nuevos.get())
+    mensaje_nuevos.config(text=" ")
+  except ValueError:
+    mensaje_nuevos.config(text="Por favor, ingrese un numero en el numero de alumnos por salon")
+  showSubmit()
+
+def studNumber():
+  try:
+    int(sv_alumnos.get())
+    mensaje_alumnos.config(text=" ")
+  except ValueError:
+    mensaje_alumnos.config(text="Por favor, ingrese un numero en el numero de alumnos por salon")
+  showSubmit()
+
 def gradesNumber():
   try:
-    mensaje.config(text="")
+    mensaje_notas.config(text=" ")
     for i in notasInput:
       for j in i:
         j.pack_forget()
+    for i in notasInputVars:
+      i[1].pack_forget()
+
     notasInput.clear()
-    for i in range(int(sv.get())):
+    notasInputVars.clear()
+    for i in range(int(sv_grades.get())):
+      sv_temp = StringVar()
+      sv_temp.trace("w", lambda name, index, mode, sv_temp=sv_temp: percentageCheck())
+      mensaje_temp = Label(top, text="")
+      notasInputVars.append([sv_temp, mensaje_temp])
+
       temp1 = Label(top, text="Nota"+str(i+1)+":")
-      temp2 = Entry(top)
+      temp2 = Entry(top, textvariable = notasInputVars[i][0])
       temp3 = [temp1, temp2]
       notasInput.append(temp3)
-    for i in notasInput:
-      for j in i:
-        j.pack()
-    print(int(sv.get()))
+    for i in range(len(notasInput)):
+        notasInput[i][0].pack()
+        notasInput[i][1].pack()
+        notasInputVars[i][1].pack()
   except ValueError:
-    mensaje.config(text="Por favor, ingrese un numero en el numero de alumnos por salon")
+    mensaje_notas.config(text="Por favor, ingrese un numero en el numero de notas")
+    print(mensaje_notas.cget("text"))
+  showSubmit()
+
+def executeAndClose():
+  run()
+  top.destroy()
+
 def openEjecutar():
+  global habiles_text
+  global nuevos_text
   global courseName
   global maxStudents
   global numberGrades
-  global sv
+  global numberHabiles
+  global numberNuevos
+  global sv_grades
+  global sv_alumnos
+  global sv_nuevos
+  global sv_habiles
   global top
-  global mensaje
+  global mensaje_notas
+  global mensaje_alumnos
+  global mensaje_nuevos
+  global mensaje_habiles
+  global notasInputVars
   global notasInput
+  global submit
   notasInput=[]
+  notasInputVars=[]
   top = Toplevel()
   top.title("Run data")
-  top.geometry("500x200")
+  top.geometry("1000x700")
+  scroll = Scrollbar(top, orient=VERTICAL)
+  scroll.pack(side=RIGHT, fill=Y)
+
   course_text = Label(top, text="Nombre de curso: ")
   courseName = Entry(top)
-  maxstudents_text = Label(top, text="Numero maximo de alumnos por salon: ")
-  maxStudents = Entry(top)
-  sv = StringVar()
-  sv.trace("w", lambda name, index, mode, sv=sv: gradesNumber())
-  number_text = Label(top, text="Numero de notas de curso: ")
-  numberGrades = Entry(top, textvariable = sv)
-  mensaje = Label(top, text="")
-  submit = Button(top, text="Submit", command=parseAndClose, state=DISABLED)
+
+  sv_alumnos = StringVar()
+  sv_alumnos.trace("w", lambda name, index, mode, sv_alumnos=sv_alumnos: studNumber())
+  maxstudents_text = Label(top, text="Número maximo de alumnos por salon: ")
+  maxStudents = Entry(top, textvariable = sv_alumnos)
+  mensaje_alumnos = Label(top, text="")
+
+  sv_grades = StringVar()
+  sv_grades.trace("w", lambda name, index, mode, sv_grades=sv_grades: gradesNumber())
+  number_text = Label(top, text="Número de notas de curso: ")
+  numberGrades = Entry(top, textvariable = sv_grades)
+  mensaje_notas = Label(top, text="")
+
+  sv_habiles = StringVar()
+  sv_habiles.trace("w", lambda name, index, mode, sv_habiles=sv_habiles: habilesNumber())
+  habiles_text = Label(top, text="Número de alumnos hábiles este ciclo: ")
+  numberHabiles = Entry(top, textvariable = sv_habiles)
+  mensaje_habiles = Label(top, text="")
+
+  sv_nuevos = StringVar()
+  sv_nuevos.trace("w", lambda name, index, mode, sv_nuevos=sv_nuevos: nuevosNumber())
+  nuevos_text = Label(top, text="Número de alumnos nuevos para el siguiente ciclo: ")
+  numberNuevos = Entry(top, textvariable = sv_nuevos)
+  mensaje_nuevos = Label(top, text="")
+
+  submit = Button(top, text="Submit", command=executeAndClose, state=DISABLED)
+
   course_text.pack(pady=(20,5))
   courseName.pack()
+
   maxstudents_text.pack(pady=(20,5))
   maxStudents.pack()
+  mensaje_alumnos.pack()
+
   number_text.pack(pady=(20,5))
   numberGrades.pack()
-  mensaje.pack()
+  mensaje_notas.pack()
   
+  habiles_text.pack(pady=(20,5))
+  numberHabiles.pack()
+  mensaje_habiles.pack()
+
+  nuevos_text.pack(pady=(20,5))
+  numberNuevos.pack()
+  mensaje_nuevos.pack()
+
+  submit.pack()
 
 BGCOLOR = "#EFF0F1"
 root = Tk()
